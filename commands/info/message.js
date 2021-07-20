c4fb3f4d32f2ebbd76ad940a8cf362a42e2cdf7a
TimRaben <87567520+TimRaben@users.noreply.github.com>
2021-07-20 13:22:28 +0200

    All Bugs Fixed - 1.02

--- commands/info/message.js
+++ commands/info/message.js
@@ -1,19 +1,19 @@
 const { MessageEmbed } = require(`discord.js`)
 
 module.exports ={
-    name: "embed",
+    name: 'embed',
     run : async(client, message) => {
-        const embed = new Discord.RichEmbed()
-.setTitle("Trispcs Appeal - Informatie")
-.setAuthor("Trispcs", "https://images-ext-1.discordapp.net/external/gNeYUX6tot0dDNTiqq8H5UnafHbeKoi4Npsn2UcJh5I/https/cdn.mee6.xyz/guild-images/862632855585423411/164cee6d0aa8e72db091b09e74223cb13e68e8703735aa044c05a3bf97a46b0e.jpeg?width=671&height=671","https://yagami.xyz")
-.setColor("#5900ff")
-.setDescription("Hey! Je kan Appealen door middel van !appeal, dan stuurt onze BOT jou in DM een vragenlijst die jij per stap moet invullen! zodra jij dit hebt gedaan en er komt GEEN volgende stap meer dan is jou Appeal succesvol ingediend! Wij zullen hem dan binnen 3 dagen behandelen en dan krijgt u persoonlijk het antwoord gestuurd! Krijg je geen DM wanneer je de commando uitvoert? Ga dan naar Instellingen > Privacy & Veiligheid > Privé Berichten van Serverleden, zet dit aan! en typ dan het Commando opnieuw! Werkt dit niet contacteer een Stafflid in PM! **Succes!**")
-.setFooter("Trispcs Community, https://images-ext-1.discordapp.net/external/gNeYUX6tot0dDNTiqq8H5UnafHbeKoi4Npsn2UcJh5I/https/cdn.mee6.xyz/guild-images/862632855585423411/164cee6d0aa8e72db091b09e74223cb13e68e8703735aa044c05a3bf97a46b0e.jpeg?width=671&height=671")
-.setImage("http://i.imgur.com/yVpymuV.png")
-.setThumbnail("https://images-ext-1.discordapp.net/external/gNeYUX6tot0dDNTiqq8H5UnafHbeKoi4Npsn2UcJh5I/https/cdn.mee6.xyz/guild-images/862632855585423411/164cee6d0aa8e72db091b09e74223cb13e68e8703735aa044c05a3bf97a46b0e.jpeg?width=671&height=671")
-        message.channel.send({embed})
+        const embed = new MessageEmbed()
+        .setTitle(`Trispcs Ban Appeals - Informatie`)
+        .setAuthor('Trispcs', "https://images-ext-1.discordapp.net/external/NORawEhVvi3eyHIu1UC4WyOprhN4EDzzAO7huG5FCdM/%3Fwidth%3D671%26height%3D671/https/media.discordapp.net/attachments/838112437239939133/866633618083807252/trispcs.jpg")
+        .setDescription('Hey! Je kan Appealen door middel van !appeal, dan stuurt onze BOT jou in DM een vragenlijst die jij per stap moet invullen! zodra jij dit hebt gedaan en er komt GEEN volgende stap meer dan is jou Appeal succesvol ingediend! Wij zullen hem dan binnen 3 dagen behandelen en dan krijgt u persoonlijk het antwoord gestuurd! Krijg je geen DM wanneer je de commando uitvoert? Ga dan naar **Instellingen > Privacy & Veiligheid > Privé Berichten van Serverleden, zet dit aan! en typ dan het Commando opnieuw! Werkt dit niet contacteer een Stafflid in PM!')
+        .setColor(`#33ace8`)
+        .setFooter('Trispcs Community', "https://images-ext-1.discordapp.net/external/NORawEhVvi3eyHIu1UC4WyOprhN4EDzzAO7huG5FCdM/%3Fwidth%3D671%26height%3D671/https/media.discordapp.net/attachments/838112437239939133/866633618083807252/trispcs.jpg")
+        .setThumbnail(`https://media.discordapp.net/attachments/838112437239939133/866633618083807252/trispcs.jpg?width=671&height=671`)
+
+        // message.channel.send()
+        // message.reply()
+
+        message.channel.send(embed)
     }
-
-
-
 }
