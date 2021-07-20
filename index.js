297b18275fc4b4d70d242cb934f716352c984a64
TimRaben <87567520+TimRaben@users.noreply.github.com>
2021-07-20 13:55:36 +0200

    reaction roles worked

--- index.js
+++ index.js
@@ -34,9 +34,9 @@ client.on('messageReactionAdd', async(reaction, user) => {
     if(reaction.partial) await reaction.fetch();
     if(user.bot) return;
     if(!reaction.message.guild) return;
-    if(reaction.message.id === '<messageid>'){
-        if(reaction.emoji.name === '<emoji>') {
-            await reaction.message.guild.members.cache.get(user.id).roles.add('<roleID>')
+    if(reaction.message.id === '867011210280042507'){
+        if(reaction.emoji.name === '✅') {
+            await reaction.message.guild.members.cache.get(user.id).roles.add('862636435408289813')
             user.send('You have obtained a role!')
         }
     }
@@ -46,9 +46,9 @@ client.on('messageReactionRemove', async(reaction, user) => {
     if(reaction.partial) await reaction.fetch();
     if(user.bot) return;
     if(!reaction.message.guild) return;
-    if(reaction.message.id === '<messageid>'){
-        if(reaction.emoji.name === '<emoji>') {
-            await reaction.message.guild.members.cache.get(user.id).roles.remove('<roleID>')
+    if(reaction.message.id === '867011210280042507'){
+        if(reaction.emoji.name === '✅') {
+            await reaction.message.guild.members.cache.get(user.id).roles.remove('862636435408289813')
             user.send('One of your roles has been removed!')
         }
     }
